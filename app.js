5ed507ba7b38dc98f737420a839fe9b655378e1c
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-09-07 13:06:56 +0400

    Added jquery via bower.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var socket = io();
 
-$('form').submit(function() {
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-});
+// $('form').submit(function() {
+//     socket.emit('chat message', $('#m').val());
+//     $('#m').val('');
+//     return false;
+// });
 
 socket.on('chat message', function(msg) {
     console.log('received emit');
@@ -13,12 +13,21 @@ socket.on('chat message', function(msg) {
 
 // Angular
 /**
-* socketChat Modul;
+* socketChat Module;
 *
 * Description
 */
 angular.module('socketChat', []);
 
 angular.module('socketChat').controller('socketCtrl', ['$scope', function($scope){
-    
+
+    $scope.message = '';
+
+    // On submit
+    $scope.submit = function() {
+        socket.emit('chat message', $scope.message);
+        $scope.message = '';
+        return false;
+    };
+
 }]);
