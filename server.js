74a30f1d7a3f0e074cac4f54fd1a997ed71ff8ef
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-09-07 13:56:28 +0400

    Clean file

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-    console.log('__dirname', __dirname);
     res.sendFile(__dirname + '/index.html');
 });
 
+// On IO Connection
 io.on('connection', function(socket) {
-    console.log('a user connected');
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
     });
+
 });
 
+// Server listening on port
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
