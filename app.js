e944fabf87b5cdbf0285e2ed3d907571d0772b10
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-09-07 12:27:32 +0400

    Introduced angular module socketChat

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ socket.on('chat message', function(msg) {
     console.log('received emit');
     $('#messages').append($('<li>').text(msg));
 });
+
+// Angular
+/**
+* socketChat Modul;
+*
+* Description
+*/
+angular.module('socketChat', []);
+
+angular.module('socketChat').controller('socketCtrl', ['$scope', function($scope){
+    
+}]);
